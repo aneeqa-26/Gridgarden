7fae9a154013044c955cd05214b609c2ff896a98
aneeqa-26 <ansarianeeqa9@gmail.com>
2025-05-08 21:30:33 +0530

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,22 @@
+var messages = {
+    languageActive: {
+      'en': 'English', },
+      next: {
+        'en': 'Next',
+    },
+    labelFooter: {
+      'en': 'Grid Garden is created by',},
+      labelLevel: {
+        'en': 'Level', },
+        labelOf: { },
+        title: {
+          'en': 'Grid Garden - A game for learning CSS grid',},
+          labelReset: {
+            'en': 'Reset', },
+            warningReset: {
+              'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.', },
+              warningUnsupported: {
+                'en': 'Oh no, Grid Garden doesn\'t work on this browser. It requires <a href="https://caniuse.com/#feat=css-grid">a browser that supports CSS grid</a>, such as the latest version of <a href="https://www.mozilla.org/firefox/">Firefox</a>, <a href="https://www.google.com/chrome/">Chrome</a>, or <a href="https://www.apple.com/safari/">Safari</a>. Use one of those to get gardening!',  },
+                flexboxFroggy: {
+                  'en': 'Want to learn more CSS? Play', },
+                };
